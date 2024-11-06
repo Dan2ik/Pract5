923657aa2c27033e0966ec50eda6fcc611fcb302
dania <daniarmendygaliev14@gmail.com>
2024-11-06 10:34:08 +0500

    добавили выбор шоколада

--- src/main/java/org/example/demo/FoodFormController.java
+++ src/main/java/org/example/demo/FoodFormController.java
@@ -1,13 +1,18 @@
 package org.example.demo;
 
+
 import javafx.collections.FXCollections;
-import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.StringConverter;
+import org.example.demo.Models.Chocolates;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -33,6 +38,25 @@ public class FoodFormController  implements Initializable {
     final String FOOD_FRUIT = "Фрукт";
     final String FOOD_CHOCOLATE = "Шоколадка";
     final String FOOD_COOKIE = "Булочка";
+    private Boolean modalResult = false;
+    // обработчик нажатия на кнопку Сохранить
+    public void onSaveClick(ActionEvent actionEvent) {
+        this.modalResult = true; // ставим результат модального окна на true
+        // закрываем окно к которому привязана кнопка
+        ((Stage)((Node)actionEvent.getSource()).getScene().getWindow()).close();
+    }
+
+    public void onCancelClick(ActionEvent actionEvent) {
+        this.modalResult = false; // ставим результат модального окна на false
+        // закрываем окно к которому привязана кнопка
+        ((Stage)((Node)actionEvent.getSource()).getScene().getWindow()).close();
+    }
+
+    // геттер для результата модального окна
+    public Boolean getModalResult() {
+        return modalResult;
+    }
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -42,9 +66,58 @@ public class FoodFormController  implements Initializable {
                 FOOD_COOKIE
         ));
         cmbFoodType.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            // вызываю новую функцию
+            updatePanes((String) newValue);
+        });
+        // вызываю новую функцию при инициализации формы
+        updatePanes("");
+        cmbChocolateType.getItems().setAll(
+                Chocolates.Type.white,
+                Chocolates.Type.black,
+                Chocolates.Type.milk
+        );
+        // чтобы типы объекты рендерились как строки
+        cmbChocolateType.setConverter(new StringConverter<Chocolates.Type>() {
+            @Override
+            public String toString(Chocolates.Type object) {
+                if (object == null) {
+                    return "";
+                }
+                switch (object) {
+                    case white:
+                        return "Белый";
+                    case black:
+                        return "Черный";
+                    case milk:
+                        return "Молочный";
+                }
+                return "";
+            }
+
+            @Override
+            public Chocolates.Type fromString(String s) {
+                return null;
+            }
+
+            /*@Override
+            public Chocolates.Type fromString(String string) {
+                // этот метод не трогаем так как наш комбобкос имеет фиксированный набор элементов
+                return null;
+            }*/
+        });
+        updatePanes("");
+
+    }
+
+
+    public void updatePanes(String value) {
+        cmbFoodType.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             this.cookiePane.setVisible(newValue.equals(FOOD_COOKIE));
+            this.cookiePane.setManaged(newValue.equals(FOOD_COOKIE));
             this.fruitPane.setVisible(newValue.equals(FOOD_FRUIT));
+            this.fruitPane.setManaged(newValue.equals(FOOD_FRUIT));
             this.chocolatePane.setVisible(newValue.equals(FOOD_CHOCOLATE));
+            this.chocolatePane.setManaged(newValue.equals(FOOD_CHOCOLATE));
         });
 
     }
